b9b8b3df6afaaa17c1074b4579916d44752f4b6b
Christopher Garbutt <garbca1@student.op.ac.nz>
2018-06-11 12:25:05 +1200

    updated spiders

--- news/news/spiders/nyt.py
+++ news/news/spiders/nyt.py
@@ -8,41 +8,55 @@ from news.items import NewsItem
 import datetime
 import urllib.parse
 import socket
+#from selenium import webdriver 
+
+
 
 class NytSpider(CrawlSpider):
     name = 'news'
     allowed_domains = ['nytimes.com']
-    start_urls = ['https://www.nytimes.com']
+    start_urls = ['http://spiderbites.nytimes.com/1999/articles_1999_02_00003.html']
 
     rules = (
-        Rule(
-            LinkExtractor(
-                allow=(r'/+(\d+)+/+(\d+)+/+(\d+)+/+'),
-                deny=('//*html[contains(@lang, "es")]', 'es', 'blogs', 'opinion', 'slideshow', 'mobile', 'imagepages', 'interactive')
-            ),
-            callback='parse_item', follow=True,
-        ),
-        #Rule(LinkExtractor(restrict_xpaths='//li[contains(@class,"SearchResults-item-3k02W")]'),
-         #   callback='parse_item'),
+        Rule(LinkExtractor(allow=('1999'),deny=('classified' ,'style', 'gst', 'key-rates', 'search', 'section', 'slideshow', 'mobile', 'imagepages', 'interactive')),callback='parse_link', follow=True,),)
+        #Rule(LinkExtractor(restrict_xpaths=('//*[@id="headlines" or @id="mainContent"]')),
+        #    callback='parse_item', follow=True),)
         #Rule(LinkExtractor(restrict_xpaths='//*[contains(@class,"headline")]'),
          #   callback='parse_item'),
-    )
+    #
 
+    
+    #def __init__(self, *a, **kw):
+        #super(NytSpider, self).__init__(*a, **kw)
+        #self.driver = webdriver.Chrome()
+    
+    def parse_link(self, response):
 
-    def parse_item(self, response):
+           
         #create the loader
         l = ItemLoader(item=NewsItem(), response=response)
         #load fields using xpath
         l.add_xpath('title', '//*[@itemprop="headline" or @class="balancedHeadline"]/text()')
-        l.add_xpath('author', '//*[@class="byline-author"]/text()')
-        l.add_xpath('article', '//*[@class="story-body-text story-content"]/text()')
-        l.add_xpath('dop', '//*[@itemprop="dateModified"]/text()')
-            
-
+        l.add_xpath('author', '//*[@class="byline-author" or @class="author creator"]/text()')
+        l.add_xpath('article', '//*[@class="story-body-text story-content" or @class="css-18sbwfn"]/text()')
+        l.add_xpath('dop', '//*[@itemprop="dateModified" or @class="css-pnci9ceqgapgq0"]/text()')
+        
+        
         #housekeeping fields
         l.add_value('url', response.url)
         #l.add_value('project', self.settings.get('BOT_NAME'))
         #l.add_value('spider', self.name)
         #l.add_value('server', socket.gethostname())
         #l.add_value('date', datetime.datetime.now())
+        #l.add_value('url', response.url)
         return l.load_item()
+
+            
+           
+        
+        
+      
+        
+
+
+
